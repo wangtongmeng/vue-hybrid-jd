ccb7b60acdcfd3c5615b5803f0cbb2e2a3663084
littlebirdflying <tongmengwang@163.com>
2019-10-18 21:48:11 +0800

    rem自适应屏幕及样式初始化

---
+++ src/assets/js/htmlFontSize.js
@@ -0,0 +1,14 @@
+// 定义最大的 fontSize
+const MAX_FONT_SIZE = 42
+
+// 监听 html 文档被解析完成的事件
+document.addEventListener('DOMContentLoaded', () => {
+  // 获取 html 标签
+  const html = document.querySelector('html')
+  // 获取根元素 fontSize 标准，屏幕宽度 / 10
+  let fontSize = window.innerWidth / 10
+  // 获取到的 fontSize 不允许超过我们定义的最大值
+  fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize
+  // 定义根元素 (html) fontSize 的大小 (rem)
+  html.style.fontSize = fontSize + 'px'
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="test">test fontSize</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
+<style lang="scss" scoped>
+@import '@css/style.scss';
+.test {
+  font-size: px2rem(16);
+}
+</style>
