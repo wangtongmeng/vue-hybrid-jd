db6a52392faadb27232911fb47550b203149d547
littlebirdflying <tongmengwang@163.com>
2019-10-19 11:35:50 +0800

    封装活动组件 Activity
    活动组件只是个容器，内容不固定，使用 slot

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="home">
-    <!-- swiper -->
-    <my-swiper :swiperImgs="swiperData" :height="swiperHeight"></my-swiper>
+    <div class="home-content">
+      <!-- swiper -->
+      <my-swiper :swiperImgs="swiperData" :height="swiperHeight"></my-swiper>
+      <!-- 520 活动 -->
+      <Activity>
+        <div class="activity-520">
+          <img v-for="(item, index) in activityDatas" :key="index" :src="item.icon" alt="">
+        </div>
+      </Activity>
+    </div>
   </div>
 </template>
 <script>
 import MySwiper from '@c/swiper/MySwiper.vue'
+import Activity from '@c/currency/Activity.vue'
+
 export default {
   components: {
-    MySwiper
+    MySwiper,
+    Activity
   },
   data() {
     return {
       swiperData: [],
-      swiperHeight: '184px'
+      swiperHeight: '184px',
+      activityDatas: []
     }
   },
   created() {
@@ -22,21 +34,49 @@ export default {
   methods: {
     // 获取数据
     initData() {
-      this.$http.get('/swiper').then(data => {
-        this.swiperData = data.list.map(item => item.icon)
-      }).catch(err => {
-        console.log(err)
+      this.$http
+        .get('/swiper')
+        .then(data => {
+          this.swiperData = data.list.map(item => item.icon)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      // 520活动数据
+      this.$http.get('/activitys').then(data => {
+        this.activityDatas = data.list
       })
     }
   }
 }
 </script>
 <style lang="scss" scoped>
+@import '@css/style.scss';
 .home {
   width: 100%;
   height: 100%;
   &-content {
     height: 100%;
+
+    .activity-520 {
+      margin-top: px2rem(-8);
+      border-top-left-radius: px2rem(8);
+      border-top-right-radius: px2rem(8);
+
+      img {
+        display: inline-block;
+        width: 33.33%;
+      }
+    }
+
+    .activity-pin-gou-jie {
+      background-color: white;
+      margin-top: $marginSize;
+
+      img {
+        width: 100%;
+      }
+    }
   }
 }
 </style>
---
+++ src/components/currency/Activity.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- 只是提供了一个空间，用来存放活动的一个内容。不关心内容 -->
+  <div class="activty">
+    <!-- 插槽，展示活动内容 -->
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@import '@css/style.scss';
+
+.activty {
+  max-height: px2rem(160);
+  position: relative;
+  z-index: 2;
+}
+</style>
