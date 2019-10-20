0d0a44a600202f8fd89d223e722adc86ea0c9d8b
littlebirdflying <tongmengwang@163.com>
2019-10-20 09:15:20 +0800

    实现倒计时组件CountDown

--- src/assets/js/filters.js
+++ src/assets/js/filters.js
@@ -22,3 +22,48 @@ Vue.filter('priceValue', function(value) {
   // 文本包含小数，result.toFixed(2)
   return result.toFixed(2)
 })
+
+/**
+ * 时间过滤器
+ */
+Vue.filter('filterTime', function(value) {
+  if (!value) {
+    return ''
+  }
+
+  // value 不是时间格式 时:分:秒
+  if (value.indexOf(':') === -1) {
+    return value
+  }
+
+  let result = ''
+  // arr[0] 小时
+  // arr[1] 分钟
+  // arr[3] 秒数
+  const arr = value.split(':')
+
+  // 对 小时、分钟、秒数 补0
+
+  // 时
+  if (parseInt(arr[0]) < 10) {
+    result = '0' + arr[0]
+  } else {
+    result = arr[0]
+  }
+
+  // 分
+  if (parseInt(arr[1]) < 10) {
+    result = result + ':0' + arr[1]
+  } else {
+    result = result + ':' + arr[1]
+  }
+
+  // 秒
+  if (parseInt(arr[2]) < 10) {
+    result = result + ':0' + arr[2]
+  } else {
+    result = result + ':' + arr[2]
+  }
+
+  return result
+})
--- src/components/seconds/Seconds.vue
+++ src/components/seconds/Seconds.vue
@@ -3,6 +3,7 @@
     <div class="seconds-wrap">
       <p class="seconds-wrap-title">京东秒杀</p>
       <!-- 倒计时模块组件 -->
+      <count-down :endHours="16"></count-down>
     </div>
     <div class="seconds-content">
       <div class="seconds-content-item" v-for="(item, index) in dataSource" :key="index">
@@ -15,7 +16,11 @@
   </div>
 </template>
 <script>
+import CountDown from '@c/seconds/CountDown.vue'
 export default {
+  components: {
+    CountDown
+  },
   props: {
     dataSource: {
       type: Array,
